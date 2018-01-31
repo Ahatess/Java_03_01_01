3022898f6a74c99e362c94bd1b08ca1c0e3f0d05
Andrius <andriuscere@gmail.com>
2018-01-31 11:00:43 +0200

    Uzduotis030101

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,21 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Iveskite skaiciu");
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if (a % 7 == 0) {
+            System.out.println("Skaicius dalinasi is 7");
+        } if (a % 5 == 0) {
+            System.out.println("Skaicius dalinasi is 5");
+        } if (a % 3 == 0) {
+            System.out.println("Skaicius dalinasi is 3");
+        } else {
+            System.out.println("Skaicius nesidalina nei is 7, nei is 5, nei is 3");
+        }
     }
 }
